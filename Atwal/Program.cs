0538be2b84d0c92c37063d38cc81828617ea2937
visualstudio <visualstudio@122-PC18>
2019-04-05 22:04:39 -0400

    Section D

--- Atwal/Program.cs
+++ Atwal/Program.cs
@@ -1,63 +1,84 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace Atwal
+namespace ConsoleApp1
 {
     class Program
     {
         ArrayList Beowulf;
+        private int countletters;
+
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
-
-
-            p.ReadTextFiles();
-
+            p.Wordskipper();
             Console.ReadLine();
-
         }
-        public void Run()
-
-        { this.ReadTextFiles(); }
+        public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
             // Read file using StreamReader. Reads file line by line
-            using (StreamReader file = new StreamReader("U:/Users/730069.STUDENT/Downloads/Beowulf.txt"))
+            using (StreamReader file = new StreamReader("U:/Users/730228/assignment 4/ConsoleApp1/ConsoleApp1/Beowulf.txt"))
             {
                 int counter = 0;
                 string ln;
-
                 while ((ln = file.ReadLine()) != null)
                 {
-
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
+                    counter++;
+
                 }
                 file.Close();
-                counter = File.ReadLines("U:/Users/730069.STUDENT/Downloads/beowulf.txt").Count();
-                Console.WriteLine($"File has {counter } lines.");
-                Console.WriteLine($"File has {counter * 10 } words.");
+                Console.WriteLine($"File has {counter} lines.");
+                Console.WriteLine($"File has {counter * 10} words.");
+
+
+            }
+        }
+        public void Wordfinder()
+        {
+            int f = 0;
+            foreach (var line in File.ReadAllLines("‪U:\Users\730069.STUDENT\Downloads\Beowulf.txt"))
+            {
+                if (line.Contains("sea") && line.Contains("fare"))
+                {
+                    f++;
+                }
+            }
+            Console.WriteLine(f);
+        }
+        public void Wordskipper()
+        {
+            int f = 0;
+            foreach (var line in File.ReadAllLines("‪U:\Users\730069.STUDENT\Downloads\Beowulf.txt"))
+            {
+                f++;
+                if (line.Contains("fare") && (!line.Contains("war")))
+                {
+                    Console.WriteLine(f);
+                }
 
             }
         }
 
         public int FindNumberOfBlankSpaces(string line)
         {
-            // https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
-            int countletters = 0;
+            // hhtp://stackovrflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
+            int countletter = 0;
             int countSpaces = 0;
-
             foreach (char c in line)
+
             {
                 if (char.IsLetter(c)) { countletters++; }
                 if (char.IsWhiteSpace(c)) { countSpaces++; }
+
             }
             return countSpaces;
         }
