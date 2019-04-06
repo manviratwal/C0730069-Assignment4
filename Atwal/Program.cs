e1ea81d9dbe598876a339c6639b46f26b4d42527
visualstudio <visualstudio@122-PC18>
2019-04-05 20:02:00 -0400

    Section A

--- Atwal/Program.cs
+++ Atwal/Program.cs
@@ -3,36 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atwal 
+namespace Atwal
 {
     class Program
     {
         ArrayList Beowulf;
-
-        static void main(string[] args)
+        static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
-        }
-        public void Run() { this.ReadTextFiles(); }
 
+
+            p.ReadTextFiles();
+
+            Console.ReadLine();
+
+        }
+        public void Run()
+
+        { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
             // Read file using StreamReader. Reads file line by line
-            using (StreamReader file = new StreamReader("U:/Users/730069.STUDENT/Downloads"))
+            using (StreamReader file = new StreamReader("U:/Users/730069.STUDENT/Downloads/Beowulf.txt"))
             {
                 int counter = 0;
                 string ln;
 
                 while ((ln = file.ReadLine()) != null)
                 {
+
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
                 }
                 file.Close();
+                counter = File.ReadLines("U:/Users/730069.STUDENT/Downloads/beowulf.txt").Count();
                 Console.WriteLine($"File has {counter } lines.");
 
             }
